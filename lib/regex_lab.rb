48ef03113af7362808ab3b30f1aa048c52eeb1ee
Justin Shaw <easyworkemail@gmail.com>
2019-05-01 12:52:08 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,8 +1,13 @@
 def starts_with_a_vowel?(word)
-
+  results = word.scan(/\A+[aeiou||AEIOU]/)  # results are ['a'] or []
+  results = results[0]
+  return results == 'a' || results == 'e' || results == 'i' || results == 'o' || results == 'u' || results == 'A' || results == 'E' || results == 'I' || results == 'O' || results == 'U'
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  p text
+  results = text.scan(/\w+ing/) #(\w+un...ing/)
+  p results
 
 end
 
