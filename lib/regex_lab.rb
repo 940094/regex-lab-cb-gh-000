6867620754ec88efc7a33cd35d5bae37ef8763aa
Justin Shaw <easyworkemail@gmail.com>
2019-05-01 13:16:01 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ end
 def first_word_capitalized_and_ends_with_punctuation?(text)
   p text
   # '^' is start of a line and '$' is end of line
-  p text.scan(/^[A-Z](...)\W$/) # we are looking for the start of a line with A-Z and any single character zero or more times followed by a non-word character followed by the end of aline
+  p text.scan(/^[A-Z].*\W$/) # we are looking for the start of a line with A-Z and any single character zero or more times followed by a non-word character followed by the end of aline
   p text.test?
 end
 
