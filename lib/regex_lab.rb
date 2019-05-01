8f4a9df5b8083ee593faa913f2b463dbe494d77c
Justin Shaw <easyworkemail@gmail.com>
2019-05-01 12:53:52 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -6,7 +6,7 @@ end
 
 def words_starting_with_un_and_ending_with_ing(text)
   p text
-  results = text.scan(\wing/) #(\w+un...ing/) #(/\w+ing/)
+  results = text.scan(/\wing/) #(\w+un...ing/) #(/\w+ing/)
   p results
 
 end
