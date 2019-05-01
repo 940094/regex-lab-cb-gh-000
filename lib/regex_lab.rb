5544f4437a0b6c7a430bf5b789a63d5ed05c4132
Justin Shaw <easyworkemail@gmail.com>
2019-05-01 12:56:48 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -5,10 +5,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  p text
   results = text.scan(/un\w+ing/) # say what we are matching literally: "we are looking for an 'un' followed by any number of characters (the '+' says we will match whatever comes before it 1 or more times) followed by 'ing'"
-  p results
-
 end
 
 def words_five_letters_long(text)
